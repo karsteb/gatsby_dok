a042cc9b275135338dc5c4c27ae7e24bd1fc5b2e
Karsten Breivik <karsten.breivik@gmail.com>
2021-02-09 19:23:12 +0100

    merge

---
+++ src/components/layout.js
@@ -0,0 +1,60 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import Menu from "./menu"    //tillegg karsten for import av meny
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+
+  //Her legges menypunktene til i taggen <Menu />
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Menu />    
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/20210208/index.md
@@ -0,0 +1,6 @@
+---
+path: "/post-one"
+date: "2021-02-08"
+title: "My second Gatsby Post"
+author: "Karsten Breivik"
+---
